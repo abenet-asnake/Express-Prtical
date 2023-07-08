10f892cf2d485399a80d7eafc6f9d289317ca11a
abenet-asnake <138355102+abenet-asnake@users.noreply.github.com>
2023-07-08 16:46:16 +0300

    creating server using port 3000

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const app =express();
 app.get('/', (req,res)=>{
     res.send('Hello World');
     console.log('Hello World');
-    
+
 });
 
+app.listen(3000, ()=>{
+    console.log('Server is running on port 3000');
+});
