a3ab827230dc528bbdf983c05563723d2fbf21cd
abenet-asnake <138355102+abenet-asnake@users.noreply.github.com>
2023-07-08 17:17:38 +0300

    Method

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
 const express= require('express'); // module returs in function 
 const app =express();
 app.get('/', (req,res)=>{
-    res.send('Hello World');
+    res.send('Hello World get server ');
     console.log('Hello World');
 
+});
+
+app.put('/', (req,res)=>{
+     res.send('I am using put replace method  ');
+});
+
+app.post('/', (req,res)=>{
+    res.send('I am using post method summit on server ');
+});
+
+app.patch('/', (req,res)=>{
+    res.send('I am using post method summit on server ');
 });
 
 app.listen(3000, ()=>{
