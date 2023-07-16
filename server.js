c3787e61baafa7a144f9ee02424c61a1db37faab
abenet-asnake <138355102+abenet-asnake@users.noreply.github.com>
2023-07-16 10:04:15 +0300

    // write method is not  request response is stop

--- server.js
+++ server.js
@@ -1,75 +1,32 @@
 const express= require('express'); // module returs in function 
 const app =express();
 let user= 'abenet';
-app.get('/', (req,res)=>{
-    res.send(`WellCome, ${user}`); 
-    console.log('Hello World');
+// Define a router
+const router = express.Router();
 
-});
-app.get('/a/:id', (req,res)=>{
-  res.send(`WellCome, ${user}`); 
-  console.log('Hello World');
+// Middleware function
+const logger = (req, res, next) => {
+  console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
+  next();
+};
 
-});
-// making dydnamic routres
-app.get(`/${user}/:id`, function (req,res){
-  res.send(`WellCome, ${user}`); 
-  console.log('This is  server ' + req.params.server);
+// Register the middleware with the router
+router.use(logger);
 
-});
-// dynamically regular expressions 
-app.get(`/a/:id([0-9]{3})`, function (req,res){
-  res.send(`WellCome, ${user}`); 
-  console.log('This Is server ');
-
-});
-app.put('/', (req,res)=>{
-     res.send('I am using put replace method  ');
+// Route handlers
+// write method is not response
+router.get('/', (req, res) => {
+  res.write('Home Page');
 });
 
-app.post('/', (req,res)=>{
-    res.send('I am using post method summit on server ');
+router.get('/about', (req, res) => {
+  res.send('About Page');
 });
 
-app.patch('/', (req,res)=>{
-    res.send('I am using post method summit on server ');
+// Register the router with the main app
+app.use('/', router);
+
+// Start the server
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
 });
-
-app.delete('/test', (req,res)=>{
-    res.send('I am using post method delete ');
-});
-app.listen(3000, ()=>{
-    console.log('Server is running on port 3000');
-});
-
-
-
-
-// // Define a router
-// const router = express.Router();
-
-// // Middleware function
-// const logger = (req, res, next) => {
-//   console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
-//   next();
-// };
-
-// // Register the middleware with the router
-// router.use(logger);
-
-// // Route handlers
-// router.get('/', (req, res) => {
-//   res.send('Home Page');
-// });
-
-// router.get('/about', (req, res) => {
-//   res.send('About Page');
-// });
-
-// // Register the router with the main app
-// app.use('/', router);
-
-// // Start the server
-// app.listen(3000, () => {
-//   console.log('Server is running on port 3000');
-// });
