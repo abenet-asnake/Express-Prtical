cd5d295eaf679c396eea15c34bcab335aa0b788e
abenet-asnake <138355102+abenet-asnake@users.noreply.github.com>
2023-08-06 10:25:13 +0300

    importing the modules

--- token.js
+++ token.js
@@ -1,6 +1,14 @@
 const express = require('express');
 const app = express();
+const jwt= require('jsonwebtoken');
+const mongoose = require('mongoose');
 const port=3000;
+app.use(express.json());
+app.get('/api/login', (req, res) => {
+    // deconstruct the request
+    const {user,password} = req.body;
+    
+});
 app.listen(port,()=>{
     console.log(`listening on ${port}`);
 });
